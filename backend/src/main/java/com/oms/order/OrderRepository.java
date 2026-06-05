37fee05958f4ce0c05a803ca39fc0d48e8e47a5f
unknown <sarveshb.bhatt@gmail.com>
2026-06-05 22:24:39 +0530

    feat: add JPA repositories for User and Order

---
+++ backend/src/main/java/com/oms/order/OrderRepository.java
@@ -0,0 +1,21 @@
+package com.oms.order;
+
+import com.oms.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByCreatedBy(User user);
+
+    List<Order> findByStatusIn(List<OrderStatus> statuses);
+
+    @Query("SELECT oi.itemName FROM OrderItem oi " +
+           "WHERE oi.order.status IN :statuses AND oi.order.id != :excludeOrderId")
+    List<String> findItemNamesInActiveOrders(
+            @Param("statuses") List<OrderStatus> statuses,
+            @Param("excludeOrderId") Long excludeOrderId);
+}
