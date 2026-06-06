c8922c00c68a84fe5c0e5faebdd888cb82d94434
Sarvesh <sarveshb.bhatt@gmail.com>
2026-06-06 18:46:25 +0530

    feat: extend OrderRepository with JpaSpecificationExecutor

--- backend/src/main/java/com/oms/order/OrderRepository.java
+++ backend/src/main/java/com/oms/order/OrderRepository.java
@@ -2,12 +2,13 @@ package com.oms.order;
 
 import com.oms.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
 
     List<Order> findByCreatedBy(User user);
 
